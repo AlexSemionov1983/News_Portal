116dabb7461c130b0de611c4a4743a5e4f207971
AlexSemionov <alexnet545@gmail.com>
2025-11-18 04:24:25 +0300

    bootstrap applied, started applying views

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-date_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному посту
+    model = Post
+    # Используем другой шаблон — post.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем пост
+    context_object_name = 'post'
