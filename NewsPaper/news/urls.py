c403acaa19192253206eb4a3400f192d5f201ca1
AlexSemionov <alexnet545@gmail.com>
2025-11-23 11:33:04 +0300

    done pagination and filters

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import PostsList, PostDetail
+from .views import PostsList, PostDetail, SearchNews
 
 urlpatterns = [
-   # path — означает путь.
-   # В данном случае путь ко всем товарам у нас останется пустым,
-   # чуть позже станет ясно почему.
-   # Т.к. наше объявленное представление является классом,
-   # а Django ожидает функцию, нам надо представить этот класс в виде view.
-   # Для этого вызываем метод as_view.
-   path('', PostsList.as_view()),
-   path('<int:pk>', PostDetail.as_view()),
+    # path — означает путь.
+    # В данном случае путь ко всем товарам у нас останется пустым,
+    # чуть позже станет ясно почему.
+    # Т.к. наше объявленное представление является классом,
+    # а Django ожидает функцию, нам надо представить этот класс в виде view.
+    # Для этого вызываем метод as_view.
+    path('', PostsList.as_view(), name='news_list'),
+    path('news/<int:pk>', PostDetail.as_view(), name='detail'),  # <-- имя добавлено
+    path('<int:pk>', PostDetail.as_view() , name='post_detail'),
+    path('search/', SearchNews.as_view(), name='news_search'),  # <-- новый путь
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -2,6 +2,7 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 class PostsList(ListView):
@@ -9,6 +10,24 @@ class PostsList(ListView):
     ordering = '-date_time'
     template_name = 'news.html'
     context_object_name = 'news'
+
+
+class SearchNews(ListView):
+    model = Post
+    ordering = '-date_time'
+    template_name = 'search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 
 class PostDetail(DetailView):
